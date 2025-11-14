01f2c3f08e7cb61012f720f30dabe1a547dbf4ac
hongyu <hongyu@imandra.ai>
2025-11-14 11:11:13 +0000

    tests

---
+++ packages/imandrax-api-models/tests/test_proto_models.py
@@ -0,0 +1,22 @@
+import os
+
+import pytest
+from google.protobuf.message import Message
+from imandrax_api import Client, url_prod
+from inline_snapshot import snapshot
+
+from imandrax_api_models.proto_models import EvalRes
+from imandrax_api_models.proto_to_dict import proto_to_dict
+
+
+@pytest.fixture
+def c() -> Client:
+    c = Client(url=url_prod, auth_token=os.environ['IMANDRAX_API_KEY'])
+    return c
+
+
+def test_eval_res(c: Client):
+    src = 'let x = 1'
+    eval_res_msg: Message = c.eval_src(src)
+    eval_res = EvalRes.model_validate(proto_to_dict(eval_res_msg))
+    assert eval_res == snapshot(EvalRes(success=True))
