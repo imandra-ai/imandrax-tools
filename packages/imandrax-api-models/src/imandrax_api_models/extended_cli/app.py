e00fe29d8a60e13ac26971a300e4f2db5793bed2
hongyu <hongyu@imandra.ai>
2025-11-20 18:27:32 +0000

    strip vg and decomp for eval

--- packages/imandrax-api-models/src/imandrax_api_models/extended_cli/app.py
+++ packages/imandrax-api-models/src/imandrax_api_models/extended_cli/app.py
@@ -3,6 +3,12 @@ import sys
 from pathlib import Path
 
 import typer
+from iml_query.processing import (
+    extract_decomp_reqs,
+    extract_instance_reqs,
+    extract_verify_reqs,
+)
+from iml_query.tree_sitter_utils import get_parser
 
 from imandrax_api_models.client import get_imandrax_client
 from imandrax_api_models.context_utils import format_eval_res
@@ -27,7 +33,16 @@ def eval(
         None,
         help='Path of the IML file to evaluate. Set to "-" to read from stdin.',
     ),
+    strip_vg_requests: bool = typer.Option(
+        True,
+        help='Whether to strip verify and instance requests before evaluating.',
+    ),
+    strip_decomp_requests: bool = typer.Option(
+        True,
+        help='Whether to decomp requests before evaluating.',
+    ),
 ):
+    # Load IML
     iml: str
     if (iml_path is None) or (iml_path == '-'):
         # Read from stdin if no path is provided
@@ -36,6 +51,15 @@ def eval(
         if not Path(iml_path).exists():
             raise typer.BadParameter(f'IML file {iml_path} does not exist')
         iml = Path(iml_path).read_text()
+
+    if strip_vg_requests or strip_decomp_requests:
+        tree = get_parser().parse(iml.encode('utf-8'))
+
+        if strip_vg_requests:
+            iml, tree, _verify_reqs = extract_verify_reqs(iml, tree)
+            iml, tree, _instance_reqs = extract_instance_reqs(iml, tree)
+        if strip_decomp_requests:
+            iml, tree, _decomp_reqs = extract_decomp_reqs(iml, tree)
 
     c = get_imandrax_client()
     eval_res = c.eval_src(src=iml)
