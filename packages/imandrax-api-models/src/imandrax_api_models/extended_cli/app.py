9847a6a7c8f2e36201062a61999a066d8628267a
hongyu <hongyu@imandra.ai>
2025-11-20 14:41:58 +0000

    optional extended cli

---
+++ packages/imandrax-api-models/src/imandrax_api_models/extended_cli/app.py
@@ -0,0 +1,32 @@
+import sys
+from pathlib import Path
+
+import typer
+
+from imandrax_api_models.client import get_imandrax_client
+from imandrax_api_models.context_utils import format_eval_res
+
+app = typer.Typer(name='ImandraX')
+
+
+@app.command(
+    name='eval',
+)
+def eval(
+    iml_path: str | None = typer.Argument(
+        None, help='Path of the IML file to evaluate'
+    ),
+):
+    iml: str
+    if iml_path is None:
+        # Read from stdin if no path is provided
+        iml = sys.stdin.read()
+    else:
+        if not Path(iml_path).exists():
+            raise typer.BadParameter(f'IML file {iml_path} does not exist')
+        iml = Path(iml_path).read_text()
+
+    c = get_imandrax_client()
+    eval_res = c.eval_src(src=iml)
+    # breakpoint()
+    typer.echo(format_eval_res(eval_res, iml))
