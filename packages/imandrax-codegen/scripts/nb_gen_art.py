ca72888be5e7427e9fbfc19940a42931f3cd9711
hongyu <hongyu@imandra.ai>
2025-12-03 11:40:10 +0000

    test(data): inline record

--- packages/imandrax-codegen/scripts/nb_gen_art.py
+++ packages/imandrax-codegen/scripts/nb_gen_art.py
@@ -4,7 +4,7 @@ import base64
 import json
 from pathlib import Path
 
-from imandrax_api import Client, url_prod
+from imandrax_api import Client, url_dev, url_prod
 from imandrax_api.lib import read_artifact_data
 from IPython.core.getipython import get_ipython
 from rich import print
@@ -32,41 +32,30 @@ dotenv.load_dotenv('../.env')
 
 
 # %%
-c = Client(auth_token=os.environ['IMANDRAX_API_KEY'], url=url_prod)
-
-eg_dir = Path.cwd().parent / 'examples' / 'iml'
-art_dir = eg_dir.parent / 'art'
+c = Client(
+    auth_token=os.environ['IMANDRAX_API_KEY'],
+    url=url_dev,
+)
 
 # %%
+iml = """\
+type shape =
+| Point
+| Circle of int
+| Rectangle of int * int
+| Triangle of {a: int; b: int; c: int}"""
+
+
 name = 'movement'
-iml_p = eg_dir / f'{name}.iml'
-iml = iml_p.read_text()
 eval_res = c.eval_src(iml)
-instance_res = c.instance_src(src='model_movement')
-instance_res = proto_to_dict(instance_res)
-art = instance_res['sat']['model']['artifact']
-with (art_dir / name / f'{name}.json').open('w') as f:
-    json.dump(art, f, indent=2)
-
+gd_res_proto = c.get_decls(names=['shape'])
+gd_res = proto_to_dict(gd_res_proto)
 
 # %%
-iml_p = eg_dir / 'primitives.iml'
-iml = iml_p.read_text()
-eval_res = c.eval_src(iml)
-instance_res = c.instance_src(src='model_my_int')
-instance_res = proto_to_dict(instance_res)
-art = instance_res['sat']['model']['artifact']
-with (art_dir / 'primitives' / 'int.json').open('w') as f:
-    json.dump(art, f, indent=2)
-
-
-eval_res = c.eval_src(iml)
-instance_res = c.instance_src(src='model_my_bool_list')
-instance_res = proto_to_dict(instance_res)
-art = instance_res['sat']['model']['artifact']
-with (art_dir / 'primitives' / 'bool_list.json').open('w') as f:
-    json.dump(art, f, indent=2)
+gd_res
 
 # %%
-# print(read_artifact_data(data=base64.b64decode(art['data']), kind=art['kind']))
-# decode_artifact(art['data'], art['kind'])
+art = gd_res['decls'][0]['artifact']
+art_data = base64.b64decode(art['data'])
+art_kind = art['kind']
+print(read_artifact_data(art_data, art_kind))
