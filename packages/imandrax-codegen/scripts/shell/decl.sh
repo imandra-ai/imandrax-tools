2af1489ac586c628b0d5c6744c0bee92fd10461f
hongyu <hongyu@imandra.ai>
2025-12-04 10:07:55 +0000

    scripts: shell

---
+++ packages/imandrax-codegen/scripts/shell/decl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+codegen() {
+    if [[ $# -ne 1 ]]; then
+        echo "Usage: $0 <decl-file>"
+        exit 1
+    fi
+    yq '.get_decls_res.decls[0].artifact' "$1" -o json \
+    | dune exec bin/parse.exe -- - - --mode decl \
+    | uv run py-gen -
+}
+
+codegen "$1"
