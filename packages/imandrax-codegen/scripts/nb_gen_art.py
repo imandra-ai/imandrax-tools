da77233d2202300f134e8ff4ac7b252931edfa63
hongyu <hongyu@imandra.ai>
2025-12-04 17:06:23 +0000

    wip: parse decl fun def

--- packages/imandrax-codegen/scripts/nb_gen_art.py
+++ packages/imandrax-codegen/scripts/nb_gen_art.py
@@ -2,10 +2,21 @@
 # %%
 import base64
 import json
+from dataclasses import asdict
 from pathlib import Path
+from typing import cast
 
 from imandrax_api import Client, url_dev, url_prod
-from imandrax_api.lib import read_artifact_data
+from imandrax_api.lib import (
+    Artifact,
+    Common_Fun_def_t_poly,
+    Common_Var_t_poly,
+    Mir_Term_term,
+    Mir_Type,
+    Mir_Type_var,
+    Ty_view_view,
+    read_artifact_data,
+)
 from imandrax_api_models.client import ImandraXAsyncClient, ImandraXClient
 from IPython.core.getipython import get_ipython
 from rich import print
@@ -66,14 +77,94 @@ let move = fun w ->
 name = 'move'
 eval_res = c.eval_src(iml)
 
+# # %%
+# tc_res = c.typecheck(iml)
+# print(tc_res)
+
 # %%
-tc_res = c.typecheck(iml)
-print(tc_res)
+from imandrax_api.lib import (
+    Common_Decl_t_poly,
+    Common_Decl_t_poly_Fun,
+    Mir_Decl,
+    Ty_view_view_Arrow,
+    Ty_view_view_Constr,
+)
+
+# %%
+gd_res = c.get_decls(names=['movement', 'move'])
+decl = [decl for decl in gd_res.decls if decl.name == 'move'][0]
+art = decl.artifact
+mir_decl: Artifact = read_artifact_data(art.data, art.kind)
+mir_decl = cast(Mir_Decl, mir_decl)
+
+# %%
+Fun_def = Common_Fun_def_t_poly[Mir_Term_term, Mir_Type]
+fun_def: Fun_def = cast(Fun_def, mir_decl.arg)
+func_name: str = fun_def.f_name.name
+
+# Var = Common_Var_t_poly[Mir_Type]
+func_ty: Mir_Type = fun_def.f_ty.ty
+func_ty_view: Ty_view_view[None, Mir_Type_var, Mir_Type] = func_ty.view
+
+
+# %%
+print(asdict(art).keys())
+
 
 # %%
 
-gd_res = c.get_decls(names=['movement'])
-gd_res
+
+def parse_decl_fun(mir_decl: Artifact) -> tuple[str, list[str]]:
+    """Parse a Mir_Decl.Fun_def artifact.
+
+    Returns:
+        tuple[str, list[str]]: The function name and the list of argument types.
+    """
+    if not isinstance(mir_decl, Common_Decl_t_poly_Fun):
+        raise TypeError(f'Expected Mir_Decl.Fun_def, got {type(mir_decl)}')
+    fun_def: Fun_def = mir_decl.arg
+    func_name: str = fun_def.f_name.name
+
+    func_ty: Mir_Type = fun_def.f_ty.ty
+    func_ty_view: Ty_view_view[None, Mir_Type_var, Mir_Type] = func_ty.view
+
+    return func_name, unpack_arrow(func_ty_view)
+
+
+def unpack_arrow(
+    ty_view: Ty_view_view[None, Mir_Type_var, Mir_Type],
+) -> list[str]:
+    """Flatten the arrow type view to a list of types (strings)."""
+    result = []
+
+    def helper(view: Any) -> None:
+        if isinstance(view, Ty_view_view_Arrow):
+            # Extract args: (annotations?, left_t, right_t)
+            _, left_t, right_t = view.args
+            # Assumption: left_t is a Mir_Type
+            left_view = left_t.view
+            if isinstance(left_view, Ty_view_view_Constr):
+                uid, _ = left_view.args
+                result.append(uid.name)
+            else:
+                raise ValueError(
+                    'Never: left of arrow type view should be a constr'
+                )
+            # Recurse on right_t (which is a Mir_Type)
+            helper(right_t.view)
+        elif isinstance(view, Ty_view_view_Constr):
+            uid, _ = view.args
+            result.append(uid.name)
+        else:
+            raise ValueError(
+                'Never: arrow type view should be either a constr or an arrow'
+            )
+
+    helper(ty_view)
+    return result
+
+
+unpack_arrow(func_ty_view)
 
 
 # %%
