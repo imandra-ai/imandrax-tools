c11826fa88d8419ef9ee6664c4602ad9d9617611
hongyu <hongyu@imandra.ai>
2025-12-05 15:08:17 +0000

    tests: add vg with attribute

--- packages/iml-query/tests/test_query.py
+++ packages/iml-query/tests/test_query.py
@@ -2,7 +2,10 @@ from inline_snapshot import snapshot
 
 from iml_query.queries import (
     DECOMP_QUERY_SRC,
+    INSTANCE_QUERY_SRC,
     VERIFY_QUERY_SRC,
+    InstanceCapture,
+    VerifyCapture,
 )
 from iml_query.tree_sitter_utils import (
     get_parser,
@@ -90,3 +93,33 @@ def test_edge_cases_empty_content():
         mk_query(VERIFY_QUERY_SRC), node=tree_simple.root_node
     )
     assert len(matches_simple) == 0
+
+
+def test_statements_with_attributes():
+    """Test that verify and instance queries exclude item attributes."""
+    iml = """\
+verify (fun x y -> x > 0) [@@by auto]
+instance (fun x -> x > 0) [@@by auto]
+"""
+    parser = get_parser()
+    tree = parser.parse(bytes(iml, encoding='utf8'))
+
+    # Test VERIFY query
+    verify_matches = run_query(mk_query(VERIFY_QUERY_SRC), node=tree.root_node)
+    assert len(verify_matches) == 1
+    verify_capture = VerifyCapture.from_ts_capture(verify_matches[0][1])
+
+    # verify_statement should include the attribute
+    assert verify_capture.verify_statement.text == b'verify (fun x y -> x > 0) [@@by auto]'
+    # verify_expr should exclude the attribute
+    assert verify_capture.verify_expr.text == b'(fun x y -> x > 0)'
+
+    # Test INSTANCE query
+    instance_matches = run_query(mk_query(INSTANCE_QUERY_SRC), node=tree.root_node)
+    assert len(instance_matches) == 1
+    instance_capture = InstanceCapture.from_ts_capture(instance_matches[0][1])
+
+    # instance_statement should include the attribute
+    assert instance_capture.instance_statement.text == b'instance (fun x -> x > 0) [@@by auto]'
+    # instance_expr should exclude the attribute
+    assert instance_capture.instance_expr.text == b'(fun x -> x > 0)'
