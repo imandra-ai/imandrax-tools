94d56232dfff48288df632550910ef19bc3db8f9
hongyu <linerwang1997@gmail.com>
2026-01-06 15:24:01 +0000

    test: update

--- packages/imandrax-codegen/test/python/test_unparse.py
+++ packages/imandrax-codegen/test/python/test_unparse.py
@@ -167,7 +167,47 @@ def test_list_operations():
         other_decomp_kwargs=input_data.other_decomp_kwargs,
     )
     code = unparse(stmts)
-    assert code == snapshot()
+    assert code == snapshot('''\
+from __future__ import annotations
+
+
+def test_1():
+    """test_1
+
+    - invariant: 0
+    - constraints:
+        - not (xs <> [])
+    """
+    result: int = list_check(xs=[])
+    expected: int = 0
+    assert result == expected
+
+
+def test_2():
+    """test_2
+
+    - invariant: List.hd xs
+    - constraints:
+        - xs <> []
+        - not ((List.tl xs) <> [])
+    """
+    result: int = list_check(xs=[0])
+    expected: int = 0
+    assert result == expected
+
+
+def test_3():
+    """test_3
+
+    - invariant: List.hd xs + List.hd (List.tl xs)
+    - constraints:
+        - (List.tl xs) <> []
+        - xs <> []
+    """
+    result: int = list_check(xs=[1, 0])
+    expected: int = 1
+    assert result == expected
+''')
 
 
 def test_complex_variant_record():
@@ -179,7 +219,68 @@ def test_complex_variant_record():
         other_decomp_kwargs=input_data.other_decomp_kwargs,
     )
     code = unparse(stmts)
-    assert code == snapshot()
+    assert code == snapshot('''\
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass
+class user:
+    id: int
+    active: status
+
+
+@dataclass
+class Active:
+    pass
+
+
+@dataclass
+class Inactive:
+    pass
+
+
+status = Active | Inactive
+
+
+def test_1():
+    """test_1
+
+    - invariant: (-1)
+    - constraints:
+        - not (u.active = Active)
+    """
+    result: int = process_user(u=user(0, Inactive()))
+    expected: int = -1
+    assert result == expected
+
+
+def test_2():
+    """test_2
+
+    - invariant: 0
+    - constraints:
+        - u.active = Active
+        - u.id <= 0
+    """
+    result: int = process_user(u=user(0, Active()))
+    expected: int = 0
+    assert result == expected
+
+
+def test_3():
+    """test_3
+
+    - invariant: u.id
+    - constraints:
+        - u.active = Active
+        - u.id >= 1
+    """
+    result: int = process_user(u=user(1, Active()))
+    expected: int = 1
+    assert result == expected
+''')
 
 
 def test_composite_tuple():
@@ -405,7 +506,67 @@ def test_variant_simple():
         other_decomp_kwargs=input_data.other_decomp_kwargs,
     )
     code = unparse(stmts)
-    assert code == snapshot()
+    assert code == snapshot('''\
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Red:
+    pass
+
+
+@dataclass
+class Green:
+    pass
+
+
+@dataclass
+class Blue:
+    pass
+
+
+color = Red | Green | Blue
+
+
+def test_1():
+    """test_1
+
+    - invariant: 1
+    - constraints:
+        - not (c = Blue)
+        - not (c = Green)
+    """
+    result: int = color_value(c=Red())
+    expected: int = 1
+    assert result == expected
+
+
+def test_2():
+    """test_2
+
+    - invariant: 2
+    - constraints:
+        - c = Green
+        - not (c = Blue)
+    """
+    result: int = color_value(c=Green())
+    expected: int = 2
+    assert result == expected
+
+
+def test_3():
+    """test_3
+
+    - invariant: 3
+    - constraints:
+        - c = Blue
+    """
+    result: int = color_value(c=Blue())
+    expected: int = 3
+    assert result == expected
+''')
 
 
 def test_option_type():
@@ -417,7 +578,60 @@ def test_option_type():
         other_decomp_kwargs=input_data.other_decomp_kwargs,
     )
     code = unparse(stmts)
-    assert code == snapshot()
+    assert code == snapshot('''\
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Generic, TypeAlias, TypeVar
+
+T = TypeVar('T')
+
+
+@dataclass
+class Some(Generic[T]):
+    value: T
+
+
+option: TypeAlias = Some[T] | None
+
+
+def test_1():
+    """test_1
+
+    - invariant: ~- Option.get opt
+    - constraints:
+        - not Is_a(None, opt)
+        - Option.get opt <= 0
+    """
+    result: int = option_value(opt=Some(0))
+    expected: int = 0
+    assert result == expected
+
+
+def test_2():
+    """test_2
+
+    - invariant: Option.get opt
+    - constraints:
+        - not Is_a(None, opt)
+        - Option.get opt >= 1
+    """
+    result: int = option_value(opt=Some(1))
+    expected: int = 1
+    assert result == expected
+
+
+def test_3():
+    """test_3
+
+    - invariant: 0
+    - constraints:
+        - Is_a(None, opt)
+    """
+    result: int = option_value(opt=None)
+    expected: int = 0
+    assert result == expected
+''')
 
 
 def test_basic():
@@ -616,7 +830,49 @@ def test_variant_with_data():
         other_decomp_kwargs=input_data.other_decomp_kwargs,
     )
     code = unparse(stmts)
-    assert code == snapshot()
+    assert code == snapshot('''\
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Circle:
+    arg0: int
+
+
+@dataclass
+class Rectangle:
+    arg0: int
+    arg1: int
+
+
+shape = Circle | Rectangle
+
+
+def test_1():
+    """test_1
+
+    - invariant: Destruct(Rectangle, 0, s) * Destruct(Rectangle, 1, s)
+    - constraints:
+        - not Is_a(Circle, s)
+    """
+    result: int = area(s=Rectangle(0, 1))
+    expected: int = 0
+    assert result == expected
+
+
+def test_2():
+    """test_2
+
+    - invariant: Destruct(Circle, 0, s) * Destruct(Circle, 0, s)
+    - constraints:
+        - Is_a(Circle, s)
+    """
+    result: int = area(s=Circle(0))
+    expected: int = 0
+    assert result == expected
+''')
 
 
 def test_primitive_int():
