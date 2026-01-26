949a392e0074daf6d66ec0dd6295fd545e323941
hongyu <linerwang1997@gmail.com>
2026-01-26 17:57:22 +0000

    top-level api for fun decom parsing

--- packages/imandrax-codegen/lib/adapters/python/lib.ml
+++ packages/imandrax-codegen/lib/adapters/python/lib.ml
@@ -69,3 +69,14 @@ let parse_decl (decl : (Term.t, Type.t) Decl.t_poly) :
       let stmts = Transform.stmts_of_sir_type_decl sir_type_decl in
       Ok stmts
   | Error msg -> Error msg
+
+(** Parse a MIR Fun_decomp.t to corresponding AST statments for test definitions *)
+let parse_fun_decomp
+    (test_format : [< `Dict | `Function ])
+    (fun_decomp : Mir.Fun_decomp.t) : Ast.stmt list =
+  let (test_suite : Sir.test_suite) =
+    Parser.Fun_decomp.parse_fun_decomp fun_decomp
+  in
+  match test_format with
+  | `Function -> test_suite |> List.map Transform.test_func_def_of_test_decl
+  | `Dict -> [ Transform.test_data_dict_of_test_suite test_suite ]
--- packages/imandrax-codegen/lib/adapters/python/lib.mli
+++ packages/imandrax-codegen/lib/adapters/python/lib.mli
@@ -1,6 +1,7 @@
 module Type = Imandrax_api_mir.Type
 module Term = Imandrax_api_mir.Term
 module Decl = Imandrax_api_mir.Decl
+module Mir = Imandrax_api_mir
 
 val parse_model :
   (Term.term, Type.t) Imandrax_api_common.Model.t_poly -> Ast.stmt
@@ -8,3 +9,7 @@ val parse_model :
 
 val parse_decl : (Term.t, Type.t) Decl.t_poly -> (Ast.stmt list, string) result
 (** Parse a MIR Decl.t to corresponding AST statments for type declaration *)
+
+val parse_fun_decomp :
+  [< `Dict | `Function ] -> Mir.Fun_decomp.t -> Ast.stmt list
+(** Parse a MIR Fun_decomp.t to corresponding AST statments for test definitions *)
