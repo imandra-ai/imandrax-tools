7cc3f67708e56f5d69f30b2dfdb1159b845cf874
hongyu <linerwang1997@gmail.com>
2026-01-28 17:53:08 +0000

    fixup! use dir env var in sexp gen

--- packages/imandrax-codegen/test/expect-test/sir-model/test_utils.ml
+++ packages/imandrax-codegen/test/expect-test/sir-model/test_utils.ml
@@ -7,13 +7,22 @@ module Term = Imandrax_api_mir.Term
 module Model = Imandrax_api_mir.Model
 module Sir = Semantic_ir
 
+
+let workspace_dir = "../../.."
+
+let art_data_dir =
+  let art_data_dir_rel_to_workspace = Sys.getenv "TEST_DATA_ART_DIR" in
+  workspace_dir ^ "/" ^ art_data_dir_rel_to_workspace
+
+let art_data_model_dir = art_data_dir ^ "/model"
+
 (* Load a Mir.model from a yaml file *)
 let load_artifact (sub_dir : string) (name : string) : (string * string * Model.t) =
   let yaml_str =
     if sub_dir |> CCString.is_empty then
-      CCIO.File.read_exn (sprintf "../../data/model/%s.yaml" name)
+      CCIO.File.read_exn (sprintf "%s/%s.yaml" art_data_model_dir name)
       else
-      CCIO.File.read_exn (sprintf "../../data/model/%s/%s.yaml" sub_dir name)
+      CCIO.File.read_exn (sprintf "%s/%s/%s.yaml" art_data_model_dir sub_dir name)
   in
   let yaml = Yaml.of_string_exn yaml_str in
   let name, iml_code =
@@ -37,22 +46,3 @@ let load_artifact (sub_dir : string) (name : string) : (string * string * Model.
 
 let parse_model (model : Model.t) : Sir.Value_assignment.t =
   Sir.Parser.Model.parse_model model
-
-(* let test_parse_model (sub_dir : string option) (name : string) : unit =
-  let (name, iml_code, model) = load_artifact sub_dir name in
-  let _app_sym, term = unpack_model model in
-  let type_annot, expr =
-    match parse_term term with
-    | Ok (type_annot, expr) -> type_annot, expr
-    | Error msg -> failwith msg
-  in
-
-  printf "Type annot:\n";
-  (match type_annot with
-  | None -> print_endline "None"
-  | Some type_annot -> print_endline (Ast.show_expr type_annot));
-  printf "\n";
-
-  printf "Expr:\n";
-  print_endline (Ast.show_expr expr);
-  () *)
