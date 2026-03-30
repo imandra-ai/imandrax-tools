fd40eaaffe20366f7adcc7077b8c1aae98574d61
hongyu <linerwang1997@gmail.com>
2026-03-30 12:17:31 +0100

    export format goal state

--- packages/goal-state/src/lib.rs
+++ packages/goal-state/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod doc;
 pub mod formatter;
-pub mod goal_state_of_po_res;
+mod goal_state_of_po_res;
 pub mod operators;
 
 pub use formatter::prettify;
+pub use goal_state_of_po_res::format_goal_state;
