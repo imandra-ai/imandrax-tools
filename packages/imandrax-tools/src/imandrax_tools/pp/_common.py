449b4335afbbae2ccd235b67a9980183d393e109
hongyu <linerwang1997@gmail.com>
2026-06-25 10:27:46 +0100

    export common types

--- packages/imandrax-tools/src/imandrax_tools/pp/_common.py
+++ packages/imandrax-tools/src/imandrax_tools/pp/_common.py
@@ -6,3 +6,16 @@ type Region = xtype.Common_Region_t_poly[Term, Type]
 type RegionMeta = xtype.Common_Region_meta[Term]
 type RegionStatus = xtype.Common_Region_status[Term, Type]
 type FunDecomp = xtype.Mir_Fun_decomp  # = Common_Fun_decomp_t_poly[Term, Type]
+type Sequent = xtype.Common_Sequent_t_poly[xtype.Mir_Term]
+type AssocList[T] = list[tuple[str, T]]
+
+__all__ = (
+    'AssocList',
+    'Term',
+    'Type',
+    'Region',
+    'RegionMeta',
+    'RegionStatus',
+    'FunDecomp',
+    'Sequent',
+)
