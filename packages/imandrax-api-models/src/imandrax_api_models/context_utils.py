98a80283889a8abb8b0856e9e2d5ea6a7a951245
hongyu <linerwang1997@gmail.com>
2026-06-26 16:14:20 +0100

    retire old untyped str formatter

--- packages/imandrax-api-models/src/imandrax_api_models/context_utils.py
+++ packages/imandrax-api-models/src/imandrax_api_models/context_utils.py
@@ -79,114 +79,20 @@ def format_code_snippet_with_loc(
 # ====================
 
 
-def format_error_msg(
-    error_msg: ErrorMessage,
-    iml_src: str | None = None,
-    max_backtrace_len: int = 0,
-) -> str:
-    locs: list[Location] = error_msg.locs or []
-    locs = [loc for loc in locs if (loc.start is not None and loc.stop is not None)]
-
-    loc_str: str | None = None
-    error_src: str | None = None
-    if locs:
-        # TODO: handle multiple locations
-        start, stop = locs[0].start, locs[0].stop
-        start = cast(Position, start)
-        stop = cast(Position, stop)
-        loc_str = f'Lines: {start.line}:{start.col}-{stop.line}:{stop.col}'
-
-        error_src = f'Error: {error_msg.msg}\n'
-        if iml_src is not None:
-            start_pos = (start.line, start.col)
-            end_pos = (stop.line, stop.col)
-            error_src = format_code_snippet_with_loc(iml_src, start_pos, end_pos)
-    else:
-        # No location information
-        error_src = error_msg.msg
-
-    res = ''
-    if loc_str:
-        res += loc_str
-        res += '\n'
-    if error_src:
-        res += error_src
-    if max_backtrace_len > 0 and error_msg.backtrace:
-        res += f'\nbacktrace: {error_msg.backtrace[:max_backtrace_len]}'
-    return res
-
-
-def format_error(
-    error: Error,
-    iml_src: str | None = None,
-    max_stack_depth: int = 3,
-) -> str:
-    err_kind = error.kind
-    top_msg: str | None = None
-
-    if error.msg is not None:
-        top_msg = format_error_msg(error.msg, iml_src)
-
-    stack_strs = (
-        [format_error_msg(msg, iml_src) for msg in error.stack[:max_stack_depth]]
-        if error.stack
-        else []
-    )
-    stack_str = '\n'.join(stack_strs)
-
-    s = ''
-    # Top message
-    s += f'{top_msg}' if top_msg else ''
-    s += f'\n<kind>{err_kind}</kind>' if err_kind else ''
-
-    # Stack trace
-    s += f'\n<stack>\n{stack_str}\n</stack>' if stack_str else ''
-
-    return s
-
-
-def format_errors(
-    non_po_errors: list[Error],
-    po_errors: list[Error],
-    iml_src: str | None = None,
-    max_errors: int = 3,
-) -> str | None:
-    # If non-PO error exist, ignore PO errors
-    is_po_error: bool = False
-    if len(non_po_errors) > 0:
-        errs = non_po_errors[:max_errors]
-    else:
-        is_po_error = True
-        errs = po_errors[:max_errors]
-
-    err_strs: list[str] = [format_error(err, iml_src) for err in errs]
-
-    def add_tag(s: str, i: int) -> str:
-        if is_po_error:
-            return f'<po_error_{i}>\n{s}\n</po_error_{i}>'
-        else:
-            return f'<error_{i}>\n{s}\n</error_{i}>'
-
-    res = ''
-    if is_po_error:
-        res += 'Proof obligation errors (inc. termination proving):\n'
-    for i, err_str in enumerate(err_strs, 1):
-        res += add_tag(err_str, i)
-        res += '\n'
-    return res
-
-
-def format_eval_output(eval_output: EvalOutput) -> str:
-    buf = ''
+def format_eval_output(eval_output: EvalOutput, error_limit: int = 1) -> JSONObject:
+    out: JSONObject = {}
     if not eval_output.success or len(eval_output.errors) > 0:
-        buf += 'Eval failed:\n'
-        for err in eval_output.errors[:1]:
-            buf += format_error(err)
-        buf += '\n'
+        out['description'] = 'Eval failed'
+        errors = eval_output.errors
+        for i, err in enumerate(errors[:error_limit]):
+            out[f'error_{i + 1}'] = format_error(err)
+        if n_omitted := len(errors) - error_limit > 0:
+            out['n_error_omitted'] = n_omitted
+
     else:
-        buf += 'Eval Output:\n'
-        buf += f'value_as_ocaml: {eval_output.value_as_ocaml!r}\n'
-    return buf
+        out['description'] = 'Eval succeeded'
+        out['value_as_ocaml'] = eval_output.value_as_ocaml
+    return out
 
 
 def _extract_internal_error(msg: str, max_len: int = 300) -> str:
@@ -250,39 +156,7 @@ def _format_unstructured_msg_errors(
     return s
 
 
-def format_eval_res(eval_res: EvalRes, iml_src: str | None = None) -> str:
-    # Check additional error in message (internal errors)
-    errs_in_eval_msg: list[str] = [
-        msg for msg in eval_res.messages if 'error' in msg.lower()
-    ]
-    has_structured_err = eval_res.has_errors
-    has_err_in_eval_msg = len(errs_in_eval_msg) > 0
-    match (has_structured_err, has_err_in_eval_msg):
-        case True, _:
-            s = ''
-            s += 'Evaluation errors:\n'
-            s += cast(str, format_errors(eval_res.errors, eval_res.po_errors, iml_src))
-            if has_err_in_eval_msg:
-                s += '\nAdditional unstructured errors:\n'
-                s += _format_unstructured_msg_errors(errs_in_eval_msg)
-            return s
-        case False, True:
-            s = 'ImandraX internal error:\n'
-            s += _format_unstructured_msg_errors(errs_in_eval_msg)
-            return s
-        case False, False:
-            s = 'Eval success!'
-            if eval_res.eval_results:
-                s += '\n'
-            for i, eval_result in enumerate(eval_res.eval_results, 1):
-                s += f'\nEval result #{i}:\n'
-                success = eval_result.success
-                s += f'- success: {success}\n'
-                s += f'- value as ocaml: {eval_result.value_as_ocaml}\n'
-            return s
-
-
-def format_error_msg_v2(
+def format_error_msg(
     error_msg: ErrorMessage,
     iml_src: str | None = None,
     max_backtrace_len: int = 0,
@@ -316,7 +190,7 @@ def format_error_msg_v2(
     return res
 
 
-def format_error_v2(
+def format_error(
     error: Error,
     iml_src: str | None = None,
     max_stack_depth: int = 3,
@@ -325,10 +199,10 @@ def format_error_v2(
     top_msg: dict[str, Any] | None = None
 
     if error.msg is not None:
-        top_msg = format_error_msg_v2(error.msg, iml_src)
+        top_msg = format_error_msg(error.msg, iml_src)
 
     stack_strs = (
-        [format_error_msg_v2(msg, iml_src) for msg in error.stack[:max_stack_depth]]
+        [format_error_msg(msg, iml_src) for msg in error.stack[:max_stack_depth]]
         if error.stack
         else []
     )
@@ -343,7 +217,7 @@ def format_error_v2(
     return out
 
 
-def format_errors_v2(
+def format_errors(
     non_po_errors: list[Error],
     po_errors: list[Error],
     iml_src: str | None = None,
@@ -357,7 +231,7 @@ def format_errors_v2(
         is_po_error = True
         errs = po_errors[:max_errors]
 
-    err_infos: list[dict[str, Any]] = [format_error_v2(err, iml_src) for err in errs]
+    err_infos: list[dict[str, Any]] = [format_error(err, iml_src) for err in errs]
 
     out: dict[str, Any] = {}
     if is_po_error:
@@ -367,7 +241,7 @@ def format_errors_v2(
     return out
 
 
-def format_eval_res_v2(eval_res: EvalRes, iml_src: str | None = None) -> JSONObject:
+def format_eval_res(eval_res: EvalRes, iml_src: str | None = None) -> JSONObject:
     # Check additional error in messages
     errs_in_eval_msg: list[str] = [
         msg for msg in eval_res.messages if 'error' in msg.lower()
@@ -378,9 +252,7 @@ def format_eval_res_v2(eval_res: EvalRes, iml_src: str | None = None) -> JSONObj
     out: JSONObject = {}
     match (has_structured_err, has_err_in_eval_msg):
         case True, _:
-            out['error'] = format_errors_v2(
-                eval_res.errors, eval_res.po_errors, iml_src
-            )
+            out['error'] = format_errors(eval_res.errors, eval_res.po_errors, iml_src)
             if has_err_in_eval_msg:
                 out['msg_errors'] = _format_unstructured_msg_errors(errs_in_eval_msg)
             return out
@@ -420,7 +292,7 @@ def format_vg_res(vg_res: VerifyRes | InstanceRes) -> JSONObject:
     if vg_res.errors:
         out['description'] = 'VG has errors'
         for i, err in enumerate(vg_res.errors, 1):
-            out[f'error_{i}'] = format_error_v2(err)
+            out[f'error_{i}'] = format_error(err)
     else:
         out['res_type'] = vg_res.res_type
         out['res'] = vg_res.res.model_dump(mode='json')
