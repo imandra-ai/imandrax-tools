b707c24e95f769235d3de69cb4a077b6cac4709a
hongyu <linerwang1997@gmail.com>
2026-07-22 17:21:15 +0100

    rec: format diagnostics with a header

--- packages/imandrax-tools/src/imandrax_tools/iml_eval_corpus/format.py
+++ packages/imandrax-tools/src/imandrax_tools/iml_eval_corpus/format.py
@@ -1,8 +1,14 @@
 from .common import BaseDiag
 
 
-def format_diagnostics(diags: list[BaseDiag], code: str | None) -> str:
+def format_diagnostics(
+    diags: list[BaseDiag],
+    code: str | None,
+    header: bool = False,
+) -> str:
     s = ''
+    if header:
+        s += f'Found {len(diags)} diagnostics:\n'
     for i, diag in enumerate(diags, 1):
         s += f'{i}. '
         s += diag.format(code=code)
